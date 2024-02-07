9ad26bc381be91f03af732881b55673802466f1a
pedrosh101 <pedro.sh101@gmail.com>
2024-02-07 00:30:39 -0300

    sus3

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,6 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
   theme: {
     extend: {
       backgroundImage: {
